41d44f92039d9a78f152d391a542a7dbd00ce9e7
vaish621 <122389160+vaish621@users.noreply.github.com>
2024-07-04 19:32:07 +0530

    Update axiosConfig.js

--- online quiz/frontend/src/components/axiosConfig.js
+++ online quiz/frontend/src/components/axiosConfig.js
@@ -1,1 +1,9 @@
+// Assuming you have a separate file for Axios configuration (axiosConfig.js)
 
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://quizmania-back.onrender.com/', // Replace with your actual deployed backend URL
+});
+
+export default instance;
