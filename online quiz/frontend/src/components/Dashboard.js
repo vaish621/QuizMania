9bef34bc7047ecbd88275255ac51726215814d7c
vaish621 <122389160+vaish621@users.noreply.github.com>
2024-07-04 19:27:04 +0530

    Update Dashboard.js

--- online quiz/frontend/src/components/Dashboard.js
+++ online quiz/frontend/src/components/Dashboard.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from '../axiosConfig';
+import axios from './axiosConfig';
 
 const Dashboard = ({ axios }) => {
   const [quizzes, setQuizzes] = useState([]);
