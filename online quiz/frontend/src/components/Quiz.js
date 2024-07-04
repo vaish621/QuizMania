3a2d8ca978fd61fae33856c362b2a378f7259553
vaish621 <122389160+vaish621@users.noreply.github.com>
2024-07-04 15:58:31 +0530

    Update Quiz.js

--- online quiz/frontend/src/components/Quiz.js
+++ online quiz/frontend/src/components/Quiz.js
@@ -11,7 +11,7 @@ const Quiz = () => {
   useEffect(() => {
     const fetchQuiz = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/api/questions/${id}`);
+        const response = await axios.get(`${window.location.origin}/api/questions/${id}`);
         setQuiz(response.data);
       } catch (err) {
         console.error('Fetch Quiz Error:', err);
