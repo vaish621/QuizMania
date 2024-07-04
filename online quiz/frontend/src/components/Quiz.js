793919625603537f46fc1130794aa39ce23dd18d
vaish621 <122389160+vaish621@users.noreply.github.com>
2024-07-04 19:28:07 +0530

    Update Quiz.js

--- online quiz/frontend/src/components/Quiz.js
+++ online quiz/frontend/src/components/Quiz.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from '../axiosConfig';
+import axios from './axiosConfig';
 
 const Quiz = ({ axios }) => {
   const { id } = useParams();
